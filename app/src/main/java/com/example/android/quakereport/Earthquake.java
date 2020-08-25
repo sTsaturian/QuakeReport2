cd44524b4d15aef22cee7b9684b15217997895c7
sTsaturian <sTsaturian@users.noreply.github.com>
2020-08-24 20:14:32 -0400

    JSON parsing
    Added functionality for parsing JSON in format as from USGS, and displaying the retrieved earthquake data in the app.

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -4,17 +4,16 @@ public class Earthquake {
 
     private String location;
     private double magnitude;
-    private String date;
+    private long timeMilliseconds;
 
     /**
      * Create a new Word object.
-     *
-     * @param location is the description of the location of the eartquake (i.e. city)
-     * @param date is the date in format *********
+     *  @param location is the description of the location of the eartquake (i.e. city)
      * @param magnitude is the magnitude on scale from 0 to 10
+     * @param timeMilliseconds is the date in format *********
      */
-    public Earthquake(String location, double magnitude, String date) {
-        this.date = date;
+    public Earthquake(String location, double magnitude, long timeMilliseconds) {
+        this.timeMilliseconds = timeMilliseconds;
         this.location = location;
         this.magnitude = magnitude;
     }
@@ -23,8 +22,8 @@ public class Earthquake {
         return magnitude;
     }
 
-    public String getDate() {
-        return date;
+    public long getTimeMilliseconds() {
+        return timeMilliseconds;
     }
 
     public String getLocation() {
--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -10,7 +10,9 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
 
@@ -38,9 +40,40 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         TextView locView = listItemView.findViewById(R.id.location);
         locView.setText(currentEQ.getLocation());
 
-        TextView dateView = listItemView.findViewById(R.id.date);
-        dateView.setText(currentEQ.getDate());
+        // Create a new Date object from the time in milliseconds of the earthquake
+        Date dateObject = new Date(currentEQ.getTimeMilliseconds());
+
+        // Find the TextView with view ID date
+        TextView dateView = (TextView) listItemView.findViewById(R.id.date);
+        // Format the date string (i.e. "Mar 3, 1984")
+        String formattedDate = formatDate(dateObject);
+        // Display the date of the current earthquake in that TextView
+        dateView.setText(formattedDate);
+
+        // Find the TextView with view ID time
+        TextView timeView = (TextView) listItemView.findViewById(R.id.time);
+        // Format the time string (i.e. "4:30PM")
+        String formattedTime = formatTime(dateObject);
+        // Display the time of the current earthquake in that TextView
+        timeView.setText(formattedTime);
 
         return listItemView;
-    }
 }
+
+    /**
+     * Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
+     */
+    private String formatDate(Date dateObject) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("LLL dd, yyyy");
+        return dateFormat.format(dateObject);
+    }
+
+    /**
+     * Return the formatted date string (i.e. "4:30 PM") from a Date object.
+     */
+    private String formatTime(Date dateObject) {
+        SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
+        return timeFormat.format(dateObject);
+    }
+
+    }
