2bb3b4cc5afe576bdec208400f8c5abe094f0dbd
sTsaturian <sTsaturian@users.noreply.github.com>
2020-08-25 15:56:57 -0400

    Added background to the magnitude view with color depending on how high is the magnitude value

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -3,6 +3,7 @@ package com.example.android.quakereport;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +11,9 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -38,8 +41,18 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         }
 
         TextView magView = listItemView.findViewById(R.id.magnitude);
-        assert currentEQ != null;
-        magView.setText(String.format("%s", currentEQ.getMagnitude()));
+        DecimalFormat threeDigits = new DecimalFormat("0.00");
+        magView.setText(String.format("%s", threeDigits.format(currentEQ.getMagnitude())));
+
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable.
+        GradientDrawable magnitudeCircle = (GradientDrawable) magView.getBackground();
+
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor(currentEQ.getMagnitude());
+
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
 
         // Split the location into two two parts
         String fullLocation = currentEQ.getLocation();
@@ -76,6 +89,44 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
 
         return listItemView;
 }
+
+    private int getMagnitudeColor(double magnitude) {
+        int color;
+        int magFloor = (int) magnitude;
+        switch(magFloor){
+            case 0:
+            case 1:
+                color = R.color.magnitude1;
+                break;
+            case 2:
+                color = R.color.magnitude2;
+                break;
+            case 3:
+                color = R.color.magnitude3;
+                break;
+            case 4:
+                color = R.color.magnitude4;
+                break;
+            case 5:
+                color = R.color.magnitude5;
+                break;
+            case 6:
+                color = R.color.magnitude6;
+                break;
+            case 7:
+                color = R.color.magnitude7;
+                break;
+            case 8:
+                color = R.color.magnitude8;
+                break;
+            case 9:
+                color = R.color.magnitude9;
+                break;
+            default:
+                color = R.color.magnitude10plus;
+        }
+        return ContextCompat.getColor(getContext(), color);
+    }
 
     /**
      * Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
